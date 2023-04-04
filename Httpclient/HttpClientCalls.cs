f4e2a824f0dead39fc85460bd8652c5d089b6dc3
Gangadharan <121157645+GangadharanSathish@users.noreply.github.com>
2023-04-04 11:20:34 +0530

    Add files via upload

---
+++ Httpclient/HttpClientCalls.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+
+namespace Httpclient
+{
+    public class HttpClientCalls
+    { 
+        static async Task Main(string[] args)
+        {
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync("https://localhost:7290/api/CustomerDetails/GetAllCustomer");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine($"Request failed with status code {response.StatusCode}");
+            }
+        }
+    }
+}
